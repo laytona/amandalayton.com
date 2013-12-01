55d5e6a9617721e5ae6053974a1ffa477b1a40e4
Amanda Layton <laytona@gmail.com>
2013-12-01 17:35:20 +0000

    Added C4, N&S and MyMusic

--- js/project-browser.js
+++ js/project-browser.js
@@ -1,4 +1,12 @@
-$(document).ready(function(){switchTo("free");$("#scroller-free-window").scrollable({easing:"swing",size:"1",items:"#scroller-free-wrap",loop:"true",next:"#button-next-project-free",navi:"#project-dots-free",naviItem:"a",keyboard:false,speed:"500"})
+$(document).ready(function(){switchTo("c4");$("#scroller-c4-window").scrollable({easing:"swing",size:"1",items:"#scroller-c4-wrap",loop:"true",next:"#button-next-project-c4",navi:"#project-dots-c4",naviItem:"a",keyboard:false,speed:"500"})
+
+
+;$("#scroller-ns-window").scrollable({easing:"swing",size:"1",items:"#scroller-ns-wrap",loop:"true",next:"#button-next-project-ns",navi:"#project-dots-ns",naviItem:"a",keyboard:false,speed:"500"})
+
+;$("#scroller-music-window").scrollable({easing:"swing",size:"1",items:"#scroller-music-wrap",loop:"true",next:"#button-next-project-music",navi:"#project-dots-music",naviItem:"a",keyboard:false,speed:"500"})
+
+;$("#scroller-free-window").scrollable({easing:"swing",size:"1",items:"#scroller-free-wrap",loop:"true",next:"#button-next-project-free",navi:"#project-dots-free",naviItem:"a",keyboard:false,speed:"500"})
+
 
 ;$("#scroller-trinity-window").scrollable({easing:"swing",size:"1",items:"#scroller-trinity-wrap",loop:"true",next:"#button-next-project-trinity",navi:"#project-dots-trinity",naviItem:"a",keyboard:false,speed:"500"})
 
@@ -12,17 +20,23 @@ $(document).ready(function(){switchTo("free");$("#scroller-free-window").scrolla
 
 ("#scroller-comm-illus-window").scrollable({easing:"swing",size:"1",items:"#scroller-comm-illus-wrap",loop:"true",next:"#button-next-project-comm-illus",navi:"#project-dots-comm-illus",naviItem:"a",keyboard:false,speed:"500"});$("#scroller-per-illus-window").scrollable({easing:"swing",size:"1",items:"#scroller-per-illus-wrap",loop:"true",next:"#button-next-project-per-illus",navi:"#project-dots-per-illus",naviItem:"a",keyboard:false,speed:"500"});$
 
+("#MainWrapper .c4 a ").bind("click",function(){switchTo("c4")});$
+
+("#MainWrapper .ns a ").bind("click",function(){switchTo("ns")});$
+
+("#MainWrapper .music a ").bind("click",function(){switchTo("music")});$
+
+("#MainWrapper .free a ").bind("click",function(){switchTo("free")});$
+
+("#MainWrapper .lbi a ").bind("click",function(){switchTo("lbi")});$
+
 ("#MainWrapper .trinity a ").bind("click",function(){switchTo("trinity")});$
 
 ("#MainWrapper .football a ").bind("click",function(){switchTo("football")});$
 
-("#MainWrapper .lbi a ").bind("click",function(){switchTo("lbi")});$
-
 ("#MainWrapper .wgsn a ").bind("click",function(){switchTo("wgsn")});$
 
 ("#MainWrapper .freelance a ").bind("click",function(){switchTo("freelance")});$
-
-("#MainWrapper .free a ").bind("click",function(){switchTo("free")});$
 
 ("#MainWrapper .comm-illus a ").bind("click",function(){switchTo("comm-illus")});$
 
@@ -31,20 +45,29 @@ $(document).ready(function(){switchTo("free");$("#scroller-free-window").scrolla
 
 var curSect="";function switchTo(a){jQuery.easing.def="easeInOutSine";h=$(".project").height();if(a==curSect){return}
 
-if(a=="free"){$(".button-next-project").hide();$("#button-next-project-free").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"0"},{duration:500});$("#project-dots-free").show(0)}
+if(a=="c4"){$(".button-next-project").hide();$("#button-next-project-c4").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"0"},{duration:500});$("#project-dots-c4").show(0)}
 
-if(a=="lbi"){$(".button-next-project").hide();$("#button-next-project-lbi").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"-"+(h)+"px"},{duration:500});$("#project-dots-lbi").show(0)}
+if(a=="ns"){$(".button-next-project").hide();$("#button-next-project-ns").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"-"+(h)+"px"},{duration:500});$("#project-dots-ns").show(0)}
 
-if(a=="trinity"){$(".button-next-project").hide();$("#button-next-project-trinity").show();$(".project-dots").hide();$("#project-dots-trinity").show(0);$("#scroller-vertical").animate({top:"-"+(h*2)+"px"},{duration:500})}
-
-
-if(a=="football"){$(".button-next-project").hide();$("#button-next-project-football").show();$(".project-dots").hide();$("#project-dots-football").show(0);$("#scroller-vertical").animate({top:"-"+(h*3)+"px"},{duration:500})}
+if(a=="music"){$(".button-next-project").hide();$("#button-next-project-music").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"-"+(h*2)+"px"},{duration:500});$("#project-dots-music").show(0)}
 
 
-if(a=="wgsn"){$(".button-next-project").hide();$("#button-next-project-wgsn").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"-"+(h*4)+"px"},{duration:500});$("#project-dots-wgsn").show(0)}
 
-if(a=="freelance"){$(".button-next-project").hide();$("#button-next-project-freelance").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"-"+(h*5)+"px"},{duration:500});$("#project-dots-freelance").show(0)}
 
-if(a=="comm-illus"){$(".button-next-project").hide();$("#button-next-project-comm-illus").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"-"+(h*6)+"px"},{duration:500});$("#project-dots-comm-illus").show(0)}
+if(a=="free"){$(".button-next-project").hide();$("#button-next-project-free").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"-"+(h*3)+"px"},{duration:500});$("#project-dots-free").show(0)}
 
-if(a=="per-illus"){$(".button-next-project").hide();$("#button-next-project-per-illus").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"-"+(h*7)+"px"},{duration:500});$("#project-dots-per-illus").show(0)}curSect=a};
+if(a=="lbi"){$(".button-next-project").hide();$("#button-next-project-lbi").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"-"+(h*4)+"px"},{duration:500});$("#project-dots-lbi").show(0)}
+
+if(a=="trinity"){$(".button-next-project").hide();$("#button-next-project-trinity").show();$(".project-dots").hide();$("#project-dots-trinity").show(0);$("#scroller-vertical").animate({top:"-"+(h*5)+"px"},{duration:500});$("#project-dots-trinity").show(0)}
+
+
+if(a=="football"){$(".button-next-project").hide();$("#button-next-project-football").show();$(".project-dots").hide();$("#project-dots-football").show(0);$("#scroller-vertical").animate({top:"-"+(h*6)+"px"},{duration:500});$("#project-dots-football").show(0)}
+
+
+if(a=="wgsn"){$(".button-next-project").hide();$("#button-next-project-wgsn").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"-"+(h*7)+"px"},{duration:500});$("#project-dots-wgsn").show(0)}
+
+if(a=="freelance"){$(".button-next-project").hide();$("#button-next-project-freelance").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"-"+(h*8)+"px"},{duration:500});$("#project-dots-freelance").show(0)}
+
+if(a=="comm-illus"){$(".button-next-project").hide();$("#button-next-project-comm-illus").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"-"+(h*9)+"px"},{duration:500});$("#project-dots-comm-illus").show(0)}
+
+if(a=="per-illus"){$(".button-next-project").hide();$("#button-next-project-per-illus").show();$(".project-dots").hide();$("#scroller-vertical").animate({top:"-"+(h*10)+"px"},{duration:500});$("#project-dots-per-illus").show(0)}curSect=a};
